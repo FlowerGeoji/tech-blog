9d3589721b64ffd29f29c3131e028da8849cec00
novell.star <novell.star@kakaoent.com>
2022-10-25 21:31:45 +0900

    카카오 애드핏 수정

--- src/components/KakaoAdfit.tsx
+++ src/components/KakaoAdfit.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useRef, useState } from "react"
 import L from "lodash/fp"
 import cn from "classnames"
 
@@ -12,15 +12,26 @@ interface IKakaoAdfitProps {
 
 const KakaoAdfit: React.FC<IKakaoAdfitProps> = function (props) {
     const { className, style, adUnit, adWidth, adHeight } = props
+    const adFitRef = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        const scr = document.createElement("script")
+        scr.type = "text/javascript"
+        scr.src = "//t1.daumcdn.net/kas/static/ba.min.js"
+        scr.setAttribute("async", "true")
+        adFitRef.current?.appendChild(scr)
+    }, [])
 
     return (
-        <ins
-            className={cn("kakao_ad_area", className)}
-            style={style}
-            data-ad-unit={adUnit}
-            data-ad-width={L.toString(adWidth)}
-            data-ad-height={L.toString(adHeight)}
-        ></ins>
+        <div className="adfit" ref={adFitRef}>
+            <ins
+                className={cn("kakao_ad_area", className)}
+                style={style}
+                data-ad-unit={adUnit}
+                data-ad-width={L.toString(adWidth)}
+                data-ad-height={L.toString(adHeight)}
+            ></ins>
+        </div>
     )
 }
 
