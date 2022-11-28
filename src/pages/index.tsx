77d82ea70fc58be6a7fbed91e0b8fe7eb1f3ab9c
novell.star <novell.star@kakaoent.com>
2022-11-28 16:44:20 +0900

    pwa 테스트

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react"
 import { graphql } from "gatsby"
 import cn from "classnames"
+import L from "lodash/fp"
 
 import Bio from "../components/Bio"
 import Layout from "../components/Layout"
@@ -11,22 +12,30 @@ import KakaoAdfit from "../components/KakaoAdfit"
 
 const BlogIndex = ({ data, location }) => {
     const siteTitle = data.site.siteMetadata?.title || `Title`
-
     const [_category, _setCategory] = useState<string>()
+    const [_defferedPrompt, _setDefferedPrompt] = useState(undefined)
     useEffect(() => {
         if (typeof window !== "undefined") {
             window.addEventListener("beforeinstallprompt", function (event) {
                 console.log("beforeinstallprompt", event)
                 event.preventDefault()
-
-                // Show the install prompt
-                event.prompt()
-                event.userChoice().then(result => {
-                    console.log({result})
-                })
+                _setDefferedPrompt(event)
             })
         }
     }, [])
+
+    const _onConfirmPromotion = function () {
+        // Show the install prompt
+        _defferedPrompt.prompt()
+        _defferedPrompt.userChoice.then(result => {
+            console.log({ result })
+        })
+        _setDefferedPrompt(undefined)
+    }
+
+    const _onCancelPromotion = function () {
+        _setDefferedPrompt(undefined)
+    }
 
     return (
         <Layout location={location} title={siteTitle}>
@@ -43,13 +52,18 @@ const BlogIndex = ({ data, location }) => {
                     "p-[1rem]",
                     "space-y-[1rem]",
                     "drop-shadow-lg",
-                    "z-50"
+                    "z-50",
+                    { hidden: L.isNil(_defferedPrompt) }
                 )}
             >
                 <span>FlowerGeoji 사이트를 설치하시겠습니까?</span>
                 <div className={cn("flex", "flex-row", "justify-end", "space-x-[1rem]")}>
-                    <button className={cn("bg-white", "text-primary", "py-[0.25rem]", "px-[1rem]", "rounded-lg")}>취소</button>
-                    <button className={cn("bg-primary", "text-white", "py-[0.25rem]", "px-[1rem]", "rounded-lg")}>확인</button>
+                    <button className={cn("bg-white", "text-primary", "py-[0.25rem]", "px-[1rem]", "rounded-lg")} onClick={_onCancelPromotion}>
+                        취소
+                    </button>
+                    <button className={cn("bg-primary", "text-white", "py-[0.25rem]", "px-[1rem]", "rounded-lg")} onClick={_onConfirmPromotion}>
+                        확인
+                    </button>
                 </div>
             </div>
             <Seo title="FlowerGeoji" />
